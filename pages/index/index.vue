f7d5110fe43351de23c17e47ec0e0972457f1c28
huxiaolux <huxiaolux@outlook.com>
2024-04-07 18:55:39 +0800

    提交信息

---
+++ pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view>
+		<view class="content" v-for="item in dataList" :key="item.id">
+			<image class="logo" :src="item.img" mode="widthFix"></image>
+			<view class="title">{{item.title}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				dataList:[]
+			}
+		},
+		onLoad() {
+			uniCloud.callFunction({
+				name:'myCloudFn1',
+				data:{
+					name:'李四',
+					color:'red',
+					nums:3
+				}
+			}).then(res=>{
+				// console.log(res.result)
+				this.dataList=res.result
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		/* align-items: center; */
+		justify-content: center;
+		margin-bottom: 20px;
+		border-bottom: 1px solid red;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+	}
+
+	
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
